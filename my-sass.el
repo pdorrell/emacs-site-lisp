6474ca810add38cbd06498e7c42ad2e6aca5ccad
Philip Dorrell <http://www.1729.com/email.html>
2013-08-09 18:24:25 +1200

    use unix-newlines for sass output

--- my-sass.el
+++ my-sass.el
@@ -7,7 +7,7 @@
     (switch-to-buffer-other-window "*sass-watch*")
     (clear-buffer)
     (stop-then-start-process "sass-watch" '*sass-watch-process* "*sass-watch*"
-			      ruby-executable (list "-S" "sass" "--watch" src-output-argument) ) ) )
+			      ruby-executable (list "-S" "sass" "--unix-newlines" "--watch" src-output-argument) ) ) )
 
 (defun run-sass-watch-command()
   (interactive)
