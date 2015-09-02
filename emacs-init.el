41014f0a57ca3661fd77213ea28392827bc2d462
Philip Dorrell <philip@thinkinghard.com>
2015-09-02 14:25:49 +1200

    prefer vertical splitting

--- emacs-init.el
+++ emacs-init.el
@@ -55,6 +55,8 @@
 
 (setenv "EMACS_PS1" "\\w> ")
 
+(setq split-height-threshold 40)
+
 (custom-set-variables
   ;; custom-set-variables was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
