542eabaf56a76ab4c29c2c8d666cf77674708a69
Philip Dorrell <philip@thinkinghard.com>
2017-04-08 19:28:03 +1200

    install idris mode

---
+++ elpa/prop-menu-20150728.418/prop-menu-autoloads.el
@@ -0,0 +1,15 @@
+;;; prop-menu-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("prop-menu.el") (22719 50055 451291 959000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; prop-menu-autoloads.el ends here
