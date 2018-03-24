a478b3e5e140da29bf3b33d3710c9b7eda8c76ca
Philip Dorrell <philip@thinkinghard.com>
2018-03-25 12:12:24 +1300

    elpa upgrades (to get idris 20180210.400 update)

---
+++ elpa/s-20180227.51/s-pkg.el
@@ -0,0 +1,1 @@
+(define-package "s" "20180227.51" "The long lost Emacs string manipulation library." 'nil :commit "5968b951bd646c003f2825f31cdf9da94d6f1cae" :keywords '("strings"))
--- elpa/web-mode-20150528.55/web-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "web-mode" "20150528.55" "major mode for editing web templates" (quote nil))
