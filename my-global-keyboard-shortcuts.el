90b0aea90b103a25af0cfda97448fb2767342d9c
Philip <philip@thinkinghard.com>
2021-08-22 11:53:11 +1200

    put global keyboard shortcuts into groups

--- my-global-keyboard-shortcuts.el
+++ my-global-keyboard-shortcuts.el
@@ -1,146 +1,166 @@
-;;-----------------------------------------------------------------
-(apply-to-list-of-arg-lists 
- 'global-set-key
- '( ([?\M-B] buffer-menu)
-    ([?\M-C] clear-buffer)
+(setq my-alphabetic-keyboard-shortcuts
+      '( ([?\M-B] buffer-menu)
+         ([?\M-C] clear-buffer)
 
-    ([?\M-d]    delete-word-at-point)
-    ([?\M-\C-d] open-this-directory)
-    ([?\M-D]    dired-default-directory)
+         ([?\M-d]    delete-word-at-point)
+         ([?\M-\C-d] open-this-directory)
+         ([?\M-D]    dired-default-directory)
 
-    ([?\C-e]    sql-execute-query-at-point)
+         ([?\C-e]    sql-execute-query-at-point)
 
-    ([?\M-F]    font-lock-mode)
-    ([?\M-\C-F] show-face-at-point)
+         ([?\M-F]    font-lock-mode)
+         ([?\M-\C-F] show-face-at-point)
 
-    ([?\C-\M-g] goto-line)
-    ([?\M-G]    git-gui-this-directory)
+         ([?\C-\M-g] goto-line)
+         ([?\M-G]    git-gui-this-directory)
 
-    ([?\M-I] filter-on-indent)
+         ([?\M-I] filter-on-indent)
 
-    ([?\M-K] kill-buffer-y-or-n)
+         ([?\M-K] kill-buffer-y-or-n)
 
-    ([?\M-L] thin-comment-line)
+         ([?\M-L] thin-comment-line)
 
-    ([?\M-M]    build-project)
-    ([?\C-\M-M] build-project-with-target)
+         ([?\M-M]    build-project)
+         ([?\C-\M-M] build-project-with-target)
 
-    ([?\M-n] revert-if-saved)
+         ([?\M-n] revert-if-saved)
 
-    ([?\M-o]    filter-on-word-at-point)
-    ([?\M-O]    toggle-filtering)
-    ([?\C-\M-o] show-project-log-buffer)
+         ([?\M-o]    filter-on-word-at-point)
+         ([?\M-O]    toggle-filtering)
+         ([?\C-\M-o] show-project-log-buffer)
 
-    ([?\M-p] visit-project-file)
-    ([?\M-P] show-position)
+         ([?\M-p] visit-project-file)
+         ([?\M-P] show-position)
 
-    ([?\M-Q] indent-sexp)
+         ([?\M-Q] indent-sexp)
 
-    ([?\M-S] start-killable-shell)
+         ([?\M-S] start-killable-shell)
 
-    ([?\C-t]    toggle-truncate-lines)
-    ([?\C-\M-t] toggle-truncate-lines)
-    ([?\M-T]    thick-comment-line)
+         ([?\C-t]    toggle-truncate-lines)
+         ([?\C-\M-t] toggle-truncate-lines)
+         ([?\M-T]    thick-comment-line)
 
-    ([?\C-u] undo)
+         ([?\C-u] undo)
 
-    ([?\C-w] search-this-dir)
+         ([?\C-w] search-this-dir)
 
-    ([?\M-z] show-messages-buffer)
+         ([?\M-z] show-messages-buffer)
+         ) )
 
-    ([?\M-0] delete-other-windows)
-    ([?\M-7] delete-window)
-    ([?\M-8] window-only-and-split)
-    ([?\M-9] split-window-vertically)
+(setq my-numeric-key-keyboard-shortcuts
+      '( ([?\M-0] delete-other-windows)
+         ([?\M-7] delete-window)
+         ([?\M-8] window-only-and-split)
+         ([?\M-9] split-window-vertically)
+         ) )
 
-    ([?\M--] other-window)
-;;-----------------------------------------------------------------
-    ([?\C-x ?k] kill-buffer-y-or-n)
+(setq my-punctuation-key-keyboard-shortcuts
+      '( ([?\M--] other-window)
+         ) )
+ 
+(setq my-multi-key-keyboard-shorcuts
+      '( ([?\C-x ?k] kill-buffer-y-or-n)
+         ) )
 
-;;-----------------------------------------------------------------
-    ([f2] my-expand-abbrev)
+(setq my-fN-key-keyboard-shortcuts
+      '( ([f2] my-expand-abbrev)
 
-    ([f3]   save-buffer)
-    ([S-f3] save-and-kill-buffer)
-    ([M-f3] save-this-buffer-and-others)
+         ([f3]   save-buffer)
+         ([S-f3] save-and-kill-buffer)
+         ([M-f3] save-this-buffer-and-others)
 
-    ([f4]     find-file-at-point)
-    ([C-f4]   find-file-at-point-full-window)
-    ([S-f4]   visit-file-line)
-    ([M-f4]   run-file-at-point)
-    ([S-M-f4] execute-this-file)
+         ([f4]     find-file-at-point)
+         ([C-f4]   find-file-at-point-full-window)
+         ([S-f4]   visit-file-line)
+         ([M-f4]   run-file-at-point)
+         ([S-M-f4] execute-this-file)
 
-    ([f5] save-mouse-selection)
+         ([f5] save-mouse-selection)
 
-    ([C-f6] start-or-end-kbd-macro)
-    ([S-f6] call-last-kbd-macro)
+         ([C-f6] start-or-end-kbd-macro)
+         ([S-f6] call-last-kbd-macro)
 
-    ([f7]     open-base-file-menu)
-    ([C-f7]   edit-emacs-customisation)
-    ([C-M-f7] open-project-file-menu)
-    ([C-S-f7] visit-sql-file)
-    ([M-f7]   open-file-menu)
-    ([S-f7]   open-file-menu-other-window)
-    ([S-M-f7] open-base-file-menu-other-window)
+         ([f7]     open-base-file-menu)
+         ([C-f7]   edit-emacs-customisation)
+         ([C-M-f7] open-project-file-menu)
+         ([C-S-f7] visit-sql-file)
+         ([M-f7]   open-file-menu)
+         ([S-f7]   open-file-menu-other-window)
+         ([S-M-f7] open-base-file-menu-other-window)
 
-    ([M-f8] run-alternate-command)
+         ([M-f8] run-alternate-command)
 
-    ([M-f9]   run-this-file)
-    ([C-M-f9] run-this-file)
-    ([S-C-f9] kill-buffer-process)
-    ([S-M-f9] run-project)
+         ([M-f9]   run-this-file)
+         ([C-M-f9] run-this-file)
+         ([S-C-f9] kill-buffer-process)
+         ([S-M-f9] run-project)
 
-    ([C-f10]   set-dual-frames)
-    ([C-M-f10] comment-region)
-    ([S-M-f10] gitk-this-directory)
+         ([C-f10]   set-dual-frames)
+         ([C-M-f10] comment-region)
+         ([S-M-f10] gitk-this-directory)
 
-    ([C-f11]   describe-variable-at-pos)
-    ([S-f11]   describe-function-at-pos)
-    ([S-C-f11] describe-key)
+         ([C-f11]   describe-variable-at-pos)
+         ([S-f11]   describe-function-at-pos)
+         ([S-C-f11] describe-key)
 
-    ([f12]   repeat-complex-command)
-    ([M-f12] project-search-for-identifier-at-point)
+         ([f12]   repeat-complex-command)
+         ([M-f12] project-search-for-identifier-at-point)
+         ) )
 
-;;-----------------------------------------------------------------
-    ([M-S-down]   point-to-bottom)
+(setq my-directional-key-keyboard-shortcuts
+      '( ([M-S-down]   point-to-bottom)
 
-    ([home]   beginning-of-line)
-    ([C-home] beginning-of-buffer)
+         ([home]   beginning-of-line)
+         ([C-home] beginning-of-buffer)
 
-    ([end]   end-of-line)
-    ([C-end] end-of-buffer)
+         ([end]   end-of-line)
+         ([C-end] end-of-buffer)
 
-    ([C-kp-left] scroll-left-a-bit)
-    ([M-left]    backward-sexp)
+         ([C-kp-left] scroll-left-a-bit)
+         ([M-left]    backward-sexp)
 
-    ([C-kp-right] scroll-right-a-bit)
-    ([M-right]    forward-sexp)
+         ([C-kp-right] scroll-right-a-bit)
+         ([M-right]    forward-sexp)
 
-    ([C-kp-up] scroll-up-one)
-    ([M-up]    find-comment-line-bwd)
-    ([M-S-up]  point-to-top)
+         ([C-kp-up] scroll-up-one)
+         ([M-up]    find-comment-line-bwd)
+         ([M-S-up]  point-to-top)
 
-    ([C-kp-down] scroll-down-one)
+         ([C-kp-down] scroll-down-one)
 
-    ([M-down] find-comment-line-fwd)
+         ([M-down] find-comment-line-fwd)
 
-;;-----------------------------------------------------------------
-    ([M-next] find-thick-comment-line-fwd)
+         ([M-next] find-thick-comment-line-fwd)
 
-    ([M-prior] find-thick-comment-line-bwd)
+         ([M-prior] find-thick-comment-line-bwd)
+         ) )
 
-;;-----------------------------------------------------------------
-    ([pause]   next-error)
-    ([S-pause] previous-error) 
+(setq my-named-key-keyboard-shortcuts
+      '( ([pause]   next-error)
+         ([S-pause] previous-error) 
+         
+         ([delete] kill-region)
+         ([insert] yank)
+         ) )
 
-    ([delete] kill-region)
-    ([insert] yank)
-;;-----------------------------------------------------------------
-    ([mouse-3]     paste-word)
-    ([C-S-mouse-3] paste-line)
-    ([M-mouse-3]   paste-filename)
-    ([S-mouse-3]   replace-word)
-    ) )
+(setq my-mouse-shortcuts
+      '( ([mouse-3]     paste-word)
+         ([C-S-mouse-3] paste-line)
+         ([M-mouse-3]   paste-filename)
+         ([S-mouse-3]   replace-word)
+         ) )
+
+(dolist (shortcut-list 
+         (list my-alphabetic-keyboard-shortcuts 
+               my-numeric-key-keyboard-shortcuts 
+               my-punctuation-key-keyboard-shortcuts 
+               my-multi-key-keyboard-shorcuts 
+               my-fN-key-keyboard-shortcuts 
+               my-directional-key-keyboard-shortcuts 
+               my-named-key-keyboard-shortcuts 
+               my-mouse-shortcuts))
+  (dolist (shorcut-args shortcut-list)
+    (apply 'global-set-key shorcut-args) ) )
 
 ;;-----------------------------------------------------------------
 ;;    ([?\M-\C-S] spanish-minor-mode)
