05dd54d80c4108c30c7dfed39f2533413948fd20
Philip <philip@thinkinghard.com>
2021-08-22 16:46:18 +1200

    remove unused executable vars

--- executables.el
+++ executables.el
@@ -1,13 +1,10 @@
 
 ;; Locations of various executables (do not edit values here, instead specify them somewhere else using setq)
 
-(defvar *firefox-executable* nil "Firefox executable")
 (defvar *external-text-editor* nil "External text editor")
 (defvar *external-html-editor* nil "External HTML editor")
 
 (defvar *git-executable* "git" "Git executable")
 (defvar *gitk-executable* "gitk" "Git executable")
-(defvar *ruby-1.8-executable* nil "Ruby 1.8 executable")
 (defvar *java-executable* "java.exe" "Java executable")
 (defvar *python-executable* "python.exe" "Python executable")
-(defvar *python-3-executable* "python.exe" "Python executable")
