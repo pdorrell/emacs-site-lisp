939d570bfa67b6f970f657f3f46c29c94f019ecf
Philip <philip@thinkinghard.com>
2021-08-28 10:49:53 +1200

    python-with-venv project type

--- my-project-defaults.el
+++ my-project-defaults.el
@@ -5,6 +5,10 @@
          (:run-this-file ( (python . (other-window base-dir "run-python" this-file)) ))
          (:run-main-file (other-window base-dir "run-python" main-file)) )
          
+        (python-with-venv
+         (:run-this-file ( (python . (other-window base-dir "run-python-in-venv" this-file)) ))
+         (:run-main-file (other-window base-dir "run-python-in-venv" main-file)) )
+         
         (regenerated-blog 
          (:search-extensions (".rb" ".html" ".rhtml" ".css"))
          (:alternate-file-or-dir-command (other-short-window-sync base-dir "run-regenerate" this-file-or-dir)) )
