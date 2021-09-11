6bbd1539a161f89b2b34628d0e62a5a1f15f2677
Philip <philip@thinkinghard.com>
2021-09-11 12:37:29 +1200

    new project setup - basically working

--- my-project.el
+++ my-project.el
@@ -5,10 +5,107 @@
 ;; Projects is a table of all the project objects, indexed by project base directory
 (defvar *projects* (make-hash-table :test 'equal))
 
+(defvar *project-by-base-directory* (make-hash-table :test 'equal))
+
+(defvar *project-definition-file-by-base-directory* (make-hash-table :test 'equal))
+
+(defvar *project-base-directory-by-directory* (make-hash-table :test 'equal))
+
 (defvar current-project nil 
   "The current project object for the project which owns the file being edited (or default directory if there is no file) in the current buffer")
 (set-default 'current-project nil)
 (make-variable-buffer-local 'current-project)
+
+(defvar current-project-base-directory nil
+  "Directory of current project")
+(set-default 'current-project-base-directory nil)
+(make-variable-buffer-local 'current-project-base-directory)
+
+(defvar current-project-definition-file nil
+  "Current project definition file")
+(set-default 'current-project-definition-file nil)
+(make-variable-buffer-local 'current-project-definition-file)
+
+(defun get-current-directory()
+  ;; Does this always work ???
+  default-directory)
+
+(defun get-current-project-base-directory()
+  (when (not current-project-base-directory)
+    (let ( (current-directory (get-current-directory)) )
+      (setq current-project-base-directory 
+            (gethash current-directory *project-base-directory-by-directory*))
+      (if (not current-project-base-directory)
+          (load-current-project-base-directory current-directory) ) ) )
+  current-project-base-directory)
+
+(defun load-current-project-base-directory (current-directory)
+  (cons-bind project-directory project-definition-file-name
+    (find-project-directory-and-definition-file current-directory)
+    (puthash current-directory project-directory *project-base-directory-by-directory*)
+    (puthash project-directory project-definition-file-name *project-definition-file-by-base-directory*)
+    (setq current-project-base-directory project-directory)
+    (setq current-project-definition-file project-definition-file-name) ) )
+
+(defun get-current-project-definition-file()
+  (when (not current-project-definition-file)
+    (get-current-project-base-directory)
+    (setq current-project-definition-file
+          (gethash current-project-base-directory *project-definition-file-by-base-directory*) ) )
+  current-project-definition-file)
+  
+(defun get-current-project()
+  (when (not current-project)
+    (get-current-project-base-directory)
+    (if current-project-base-directory
+        (setq current-project
+              (get-project-for-base-directory current-project-base-directory) ) ) )
+  current-project)
+
+(defun get-project-for-base-directory (base-directory)
+  (let ( (project (gethash base-directory *project-by-base-directory*)) )
+    (message "project from hash = %S" project)
+    (if (not project)
+        (let ( (project-definition-file (gethash base-directory *project-definition-file-by-base-directory*)) )
+          (when project-definition-file
+              (load-project-from-definition-file base-directory project-definition-file)
+              (setq project (gethash base-directory *project-by-base-directory*)) ) ) )
+    project) )
+
+(defun load-project-from-definition-file (base-directory project-definition-file)
+  (let ( (*project-base-directory* base-directory) )
+    (load project-definition-file) ) )
+
+(get-project-for-base-directory default-directory)
+
+(defvar *project-definition-file-names* '("_project.el" "__project.el")
+  "Possible names for project definition files")
+
+(defvar *project-marker-subdirectories* '(".git")
+  "Sub-directories, if they exist, which mark the base directory of a project")
+
+(defun-getting-value find-project-directory-and-definition-file(directory)
+  "Return the project directory and project definition file for a directory as a cons pair - the directory is nil if no project marker
+is found at all, the definition file is nil if the project is defined by a sub-directory marker"
+  (let ( (dir-to-check directory) )
+    (while (not (equal dir-to-check "/"))
+      (dolist (project-definition-file-name *project-definition-file-names*)
+        (let ( (definition-file (concat dir-to-check project-definition-file-name)) )
+             (if (file-exists-p definition-file)
+                 (return-value (cons dir-to-check definition-file)) ) ) )
+      (dolist (project-marker-subdirectory *project-marker-subdirectories*)
+        (let ( (sub-directory (concat dir-to-check project-marker-subdirectory)) )
+          (if (file-exists-p sub-directory)
+              (return-value (cons dir-to-check nil)) ) ) )
+      (setq dir-to-check (file-name-directory (directory-file-name dir-to-check))) ) )
+  (return-value (cons nil nil)) )
+
+(run-test-check-expected-result
+ (find-project-directory-and-definition-file (test-file "test-project/src/subdir/"))
+ (cons (test-file "test-project/") (test-file "test-project/_project.el")) )
+
+  
+
 
 (defun create-project (key-value-pairs &optional existing-project)
   "Create (or re-create) a project object from given key/value pairs, optionally re-using an existing project if supplied"
@@ -44,34 +141,6 @@ other wise the current directory for the buffer)."
 
 (defvar *project-base-directory* nil 
   "A variable which stores the current project base directly, while loading an existing project file. ")
-
-(defvar *project-definition-file-names* '("_project.el" "__project.el")
-  "Possible names for project definition files")
-
-(defvar *project-marker-subdirectories* '(".git")
-  "Sub-directories, if they exist, which mark the base directory of a project")
-
-(file-name-directory (directory-file-name "/home/"))
-
-(defun-getting-value find-project-directory-and-definition-file(directory)
-  "Return the project directory and project definition file for a directory as a cons pair - the directory is nil if no project marker
-is found at all, the definition file is nil if the project is defined by a sub-directory marker"
-  (let ( (dir-to-check directory) )
-    (while (not (equal dir-to-check "/"))
-      (dolist (project-definition-file-name *project-definition-file-names*)
-        (let ( (definition-file (concat dir-to-check project-definition-file-name)) )
-             (if (file-exists-p definition-file)
-                 (return-value (cons dir-to-check definition-file)) ) ) )
-      (dolist (project-marker-subdirectory *project-marker-subdirectories*)
-        (let ( (sub-directory (concat dir-to-check project-marker-subdirectory)) )
-          (if (file-exists-p sub-directory)
-              (return-value (cons dir-to-check nil)) ) ) )
-      (setq dir-to-check (file-name-directory (directory-file-name dir-to-check))) ) )
-  (return-value (cons nil nil)) )
-
-(run-test-check-expected-result
- (find-project-directory-and-definition-file (test-file "test-project/src/subdir/"))
- (cons (test-file "test-project/") (test-file "test-project/_project.el")) )
 
 (defun get-current-project-dir-and-file()
   "Return the project directory and project file as a cons pair, or nil if no project file can be found."
@@ -115,6 +184,7 @@ is found at all, the definition file is nil if the project is defined by a sub-d
   (let ( (*project-base-directory* (if load-file-name *project-base-directory* (get-directory-for-project))) )
     (let ( (project (create-project key-value-pairs (gethash *project-base-directory* *projects*))) )
       (puthash :base-directory *project-base-directory* project)
+      (puthash *project-base-directory* project *project-by-base-directory*)
       (puthash *project-base-directory* project *projects*) ) ) )
 
 (defun find-current-project ()
@@ -142,7 +212,7 @@ is found at all, the definition file is nil if the project is defined by a sub-d
 
 (defun project-value (key &optional default)
   "Get the value for KEY in the current project, or from the default project if there is no current project."
-  (let* ( (project (current-project) )
+  (let* ( (project (get-current-project) )
 	  (value (gethash key project)) )
     (if (not value)
 	(if default
