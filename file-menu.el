55d1a3fced3a1dcceff4004e5130b4b45afb684d
Philip <philip@thinkinghard.com>
2021-09-24 09:27:56 +1200

    docs

--- file-menu.el
+++ file-menu.el
@@ -1,6 +1,6 @@
 ;; Copyright (C) 2001 Philip Dorrell
 
-(defvar file-menu-mode-map nil)
+(defvar file-menu-mode-map nil  "Key map for file-menu-mode")
 (if file-menu-mode-map
     nil
   (setq file-menu-mode-map (make-sparse-keymap))
@@ -8,10 +8,13 @@
 (define-key file-menu-mode-map [f4] 'open-menu-or-file-at-point)
 
 (defun get-file-menu-file-name (file-name)
+  "Get the file-menu file name of FILE-NAME, but only if FILE-NAME ends with '/'"
   (if (string-ends-with file-name "/")
       (expand-file-name "_" file-name) ) )
 
 (defun open-menu-or-file-at-point()
+  "Open file name at point, with special case - if it's a directory ending in '/',
+   open the file menu in that directory, if it exists"
   (interactive)
   (let* ( (filename-at-point (file-menu-filename-at-point))
           (file-menu-file-name 
@@ -22,6 +25,7 @@
       (find-file filename-at-point) ) ) )
                                        
 (defun file-menu-mode ()
+  "Mode for file menus, ie text files with file names, written optionally in a hierarchical mode"
   (setq major-mode 'file-menu-mode
 	mode-name "File Menu")
   (setq filename-at-point-function 'file-menu-filename-at-point)
@@ -29,6 +33,8 @@
   )
 
 (defun word-start-pos-in-line (line pos)
+  "Find the starting position of a word in LINE containing character at POS starts,
+   with a 'word' defined as a sequence of non-space characers"
   (if (>= pos (length line))
       nil
     (if (= ?  (aref line pos))
@@ -39,20 +45,26 @@
 	start) ) ) )
 
 (defun word-starting-at (line start)
+  "Get the word starting at position START in LINE, where a 
+   with a 'word' defined as a sequence of non-space characers"
   (let ( (end start) (len (length line)) )
     (while (and (< end len) (not (= ? (aref line end))))
       (setq end (1+ end)) )
     (substring line start end)))
 
 (defun word-at-pos-in-line (line pos)
+  "Get the word from LINE containing character at POS,
+   with a 'word' defined as a sequence of non-space characers"
   (let ( (start (word-start-pos-in-line line pos)) )
     (if start
 	(word-starting-at line start) ) ) )
 
 (defun to-next-tab-stop (pos)
+  "From POS in current buffer, return position of next tab stop"
   (* tab-width (1+ (/ pos tab-width))) )
 
 (defun tabs-to-spaces (line)
+  "Taking into account tab stops, convert all tabs in LINE to spaces"
   (let ( (len 0)
 	 (line-len (length line)) ch out-line)
     (dotimes (i line-len)
@@ -72,12 +84,13 @@
     out-line) )
 
 (defun first-non-space-pos-in-line (line)
+  "Get position of first non-space character in LINE"
   (let ( (start 0) (len (length line)) )
     (while (and (< start len) (= (aref line start) ? ))
       (setq start (1+ start)))
     start) )
 
-(first-non-space-pos-in-line "   jim")
+(run-test-check-expected-result (first-non-space-pos-in-line "   jim") 3)
 
 (defun file-menu-read-treed-name ()
   (save-excursion
