cd71bb7f94f4e9dc5698e73be7fdb6317d3afa6d
Philip <philip@thinkinghard.com>
2024-12-03 19:14:26 +1300

    lean4 updates

--- my-lean4.el
+++ my-lean4.el
@@ -1,16 +1,20 @@
 (require 'lean4-mode)
 
 (defun lean4-mode-hook-function ()
-  (setq programming-language 'lean4)
-  )
+  (setq programming-language 'lean4) )
 
 (add-hook 'lean4-mode-hook 'lean4-mode-hook-function)
+
+(define-key lean4-mode-map [f8] 'lean4-toggle-info)
 
 (set-abbrev-language 'lean4)
 
 (set-abbrevs
  'lean4
  '(
+   ("as" "assumption")
    ("ch" "#check ")
    ("d" "def ")
    ("e" "#eval ") ) )
+
+
