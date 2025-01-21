613355defc1e5660c2f18575b39f409a26d94683
Philip Dorrell <philip@thinkinghard.com>
2025-01-21 18:54:03 +1300

    retain "help" key fix for mac emacs

--- mac-keypad-fixes.el
+++ mac-keypad-fixes.el
@@ -1,17 +1,21 @@
-(defun fix-keypad-key-mappings()
-  (let ( (keypad-fixes
-          '( ("kp-0" . "insert")
-             ("kp-1" . "end")
-             ("kp-2" . "down")
-             ("kp-3" . "next")
-             ("kp-4" . "left")
-             ("kp-5" . "begin")
-             ("kp-6" . "right")
-             ("kp-7" . "home")
-             ("kp-8" . "up")
-             ("kp-9" . "prior")
-             ("help" . "insert") ) )
-         (prefixes '("" "C-" "M-" "S-" "C-M-" "C-S-" "M-S-" "C-M-S-")) )
+(setq *all-keypad-fixes*
+      '( ("kp-0" . "insert")
+         ("kp-1" . "end")
+         ("kp-2" . "down")
+         ("kp-3" . "next")
+         ("kp-4" . "left")
+         ("kp-5" . "begin")
+         ("kp-6" . "right")
+         ("kp-7" . "home")
+         ("kp-8" . "up")
+         ("kp-9" . "prior")
+         ("help" . "insert") ) )
+
+(setq *help-keypad-fix*
+      '( ("help" . "insert") ) )
+
+(defun fix-keypad-key-mappings(keypad-fixes)
+  (let ( (prefixes '("" "C-" "M-" "S-" "C-M-" "C-S-" "M-S-" "C-M-S-")) )
     (dolist (keypad-fix keypad-fixes)
       (let ( (in-value (car keypad-fix))
              (out-value (cdr keypad-fix)) )
@@ -19,4 +23,4 @@
           (global-set-key (kbd (concat "<" prefix in-value ">")) 
                           (kbd (concat "<" prefix out-value ">"))) ) ) ) ) )
 
-(fix-keypad-key-mappings)
+(fix-keypad-key-mappings *help-keypad-fix*)
