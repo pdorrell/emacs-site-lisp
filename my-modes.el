e3b1fd598e77dfdb19d08e5c46faf95c1f4de431
Philip <philip@thinkinghard.com>
2025-05-28 21:23:40 +1200

    remove all non-bespoke modes from my-modes.el

--- my-modes.el
+++ my-modes.el
@@ -12,23 +12,9 @@
 
 (apply-to-list-of-arg-lists 'set-file-name-mode
   '( ("_"        file-menu-mode)
-     ("rakefile" ruby-mode)
-     ("Rakefile" ruby-mode)
      ) )
 
 (apply-to-list-of-arg-lists 'set-extension-mode
-  '( (".egg"  archive-mode)
-     (".erb"  web-mode)
-     (".html" web-mode)
-     (".js"   javascript-mode)
-     (".json" javascript-mode)
-     (".jsx"  rjsx-mode)
-     (".rb"   ruby-mode)
-     (".scss" css-mode)
-     (".song" melody-scripter-mode)
-     (".sql"  sql-mode)
-     (".ts"   typescript-mode)
-     (".tsx"  rjsx-mode)
-     (".yml"  yaml-mode)
+  '( (".song" melody-scripter-mode)
      (".file-menu" file-menu-mode)
      ) )
