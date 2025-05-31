fe0c80ee66d6557003a896671c4c3d3a8d17eaf5
Philip <philip@thinkinghard.com>
2025-05-31 21:31:20 +1200

    use best modes where treesitter mode doesn't exist (in emacs 29)

--- my-treesitter.el
+++ my-treesitter.el
@@ -1,24 +1,17 @@
 ;; Add TypeScript-specific sources to the default list
-(add-to-list 'treesit-language-source-alist 
-             '(typescript "https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src"))
-(add-to-list 'treesit-language-source-alist 
-             '(tsx "https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src"))
 
-;; Install if not available
-(unless (treesit-language-available-p 'typescript)
-  (treesit-install-language-grammar 'typescript))
-(unless (treesit-language-available-p 'tsx)
-  (treesit-install-language-grammar 'tsx))
+(defvar *treesitter-sources* 
+  '(
+    (typescript "https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src")
+    (tsx "https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src")
+    (yaml "https://github.com/ikatyang/tree-sitter-yaml")
+  ) )
 
+(dolist (treesitter-source *treesitter-sources*)
+  (add-to-list 'treesit-language-source-alist treesitter-source) )
 
-(add-to-list 'treesit-language-source-alist 
-             '(scss "https://github.com/serenadeai/tree-sitter-scss"))
+(defvar *treesitter-langs* '(bash typescript tsx python javascript css html json toml yaml))
 
-;; Install if not available
-(unless (treesit-language-available-p 'scss)
-  (treesit-install-language-grammar 'scss))
-
-(treesit-install-language-grammar 'scss)
-
-(treesit-language-available-p 'scss)
-
+(dolist (lang *treesitter-langs*)
+  (unless (treesit-language-available-p lang)
+    (treesit-install-language-grammar lang) ) )
