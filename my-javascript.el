ecbb5c5cef559d928d54e6baa26a56941a72f24c
Philip <philip@thinkinghard.com>
2025-06-01 16:41:32 +1200

    need require compile in my-javascript

--- my-javascript.el
+++ my-javascript.el
@@ -1,5 +1,5 @@
 ;; Copyright (C) 2000-2013 Philip Dorrell
-
+(require 'compile)
 ;;========================================================================
 (setq javascript-word-table
       (make-alpha-table letters-digits-string "_$") )
